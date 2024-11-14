08d3a60b517f4c453edbdd91225c2eb7da49965c
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-11-13 23:16:01 -0300

    updated

---
+++ src/app/login/page.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function DialogDemo() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Login</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            Area Administrativa
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input
+              id="email"
+              defaultValue="exemplo@gmail.com"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="senha" className="text-right">
+              Senha
+            </Label>
+            <Input
+              id="senha"
+              defaultValue="*******************"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Entrar</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import { Button } from "@/components/ui/button"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 export default function Home() {
   return  (
-      <div className="bg-black flex items-center justify-center h-screen">
-        <Button>Click me</Button>
+      <div className="bg-slate-900 flex items-center justify-center h-screen">
+        <Sheet>
+          <SheetTrigger>
+            <button className="bg-slate-800 text-white p-4 px-10 rounded-lg">Open</button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px] bg-slate-800 border-none text-white">
+            <SheetHeader>
+              <SheetTitle className="text-white mt-20 text-center text-2xl">Tela Administrativa</SheetTitle>
+              <SheetDescription>
+                <form className="flex gap-2 flex-col mt-6">
+                   <input className="bg-slate-600 p-3 rounded-md" type="text" placeholder="E-mail" />
+                   <input className="bg-slate-600 p-3 rounded-md" type="text" placeholder="E-mail" />
+                   <div className="btn-group flex gap-3 mt-5">
+                    <button className="p-3 flex-1 rounded-md text-white  bg-green-700">Entrar</button>
+                    <button className="p-3 flex-1 rounded-md text-white  bg-blue-700">Cadastrar</button>
+                   </div>
+                </form>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
   );
 }
