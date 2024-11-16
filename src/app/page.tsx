5c9227177e1d334758549f1abfc99377e1f6fd9d
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-11-15 21:18:43 -0300

    controle de estado e estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,48 @@
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
+import { useState } from "react";
 
 export default function Home() {
-  return  (
-      <div className="bg-slate-900 flex items-center justify-center h-screen">
-        <Sheet>
-          <SheetTrigger>
-            <button className="bg-slate-800 text-white p-4 px-10 rounded-lg">Login</button>
-          </SheetTrigger>
-          <SheetContent className="w-[400px] sm:w-[540px] bg-slate-800 border-none text-white">
-            <SheetHeader>
-              <SheetTitle className="text-white mt-20 text-center text-2xl">Tela Administrativa</SheetTitle>
-              <SheetDescription>
-                <form className="flex gap-2 flex-col mt-6">
-                   <input className="bg-slate-600 p-3 rounded-md" type="text" placeholder="E-mail" />
-                   <input className="bg-slate-600 p-3 rounded-md" type="text" placeholder="E-mail" />
-                   <div className="btn-group flex gap-3 mt-5">
-                    <button className="p-3 flex-1 rounded-md text-white  bg-green-700">Entrar</button>
-                    <button className="p-3 flex-1 rounded-md text-white  bg-blue-700">Cadastrar</button>
-                   </div>
-                </form>
-              </SheetDescription>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
-      </div>
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  return (
+    <div className="bg-slate-950 flex items-center justify-center h-screen">
+
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline">Login</Button>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col items-center bg-slate-800 text-white border-none">
+          <SheetHeader>
+            <SheetTitle className="mt-20 text-white">Seja bem vindo.</SheetTitle>
+            <SheetDescription className="text-white">
+              Nossa area para membros
+            </SheetDescription>
+          </SheetHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="email" className="text-right"> E-mail </Label>
+              <Input id="email" className="col-span-3" value={email} onChange={ e=> setEmail(e.target.value)} />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="senha" className="text-right"> Senha: </Label>
+              <Input id="senha" className="col-span-3" type="password" value={password} onChange={ e => setPassword(e.target.value)} />
+            </div>
+          </div>
+          <SheetFooter>
+            <div className="btn-group flex gap-2">
+              <SheetClose asChild>
+                <button className="bg-blue-600 hover:bg-blue-700 w-28 text-white p-1 rounded-md">Entrar</button>
+              </SheetClose>
+              <button className="bg-green-600 hover:bg-green-700 text-white p-1 w-28 rounded-md">Cadastrar ?</button>
+            </div>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
   );
 }
