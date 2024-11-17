0137075bda5c605fa45a4e9be2b479af160238fe
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-11-16 22:18:43 -0300

    updated

---
+++ src/app/users/add-user.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { FormEvent, useState } from "react"
+
+export function AddUser(){
+    const [name, setName] = useState('')
+
+    async function handleCreateUser(event: FormEvent) {
+        event.preventDefault()
+        if(!name) {
+            return
+        }
+        await fetch('http://localhost:8080/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                name
+            })
+        })
+        alert(" Adicionado com sucesso ")
+    }
+    return(
+        <form onSubmit={handleCreateUser} className="bg-slate-800 text-white h-20 flex items-center justify-center gap-2 p-5" >
+            <input type="text" placeholder="Nome do usuário" className="p-2 text-black rounded-md flex-1" 
+                value={name} onChange={ e => setName( e.target.value ) }
+            />
+            <button className="bg-green-700 p-2 px-5 rounded-md">Adicionar</button>
+        </form>
+    )
+}
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,19 @@
-export default function Users(){
+import { AddUser } from "./add-user"
+
+export default async function Users(){
+    const response = await fetch('http://localhost:8080/users')
+    const data = await response.json()
     return(
-        <div>
-            <h1>Users</h1>
+        <div className="bg-slate-900 h-screen text-white p-5">
+            <AddUser />
+            <h1 className="text-center text-3xl mt-5">Users</h1>
+            {
+                data.map(( item: any ) => (
+                    <ul key={ item.id }>
+                        <li> { item.name } </li>
+                    </ul>
+                ))
+            }
         </div>
     )
 }
